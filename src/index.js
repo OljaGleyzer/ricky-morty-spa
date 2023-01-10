7b0bf0fe8cda99e20e11f698647347383b40bb3b
OljaGleyzer <o.gleyzer@gmail.com>
2023-01-10 15:39:18 +0100

    new component NavBar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Cards from './Cards';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './NavBar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavBar/>
     <App />
     <Cards /> 
   </React.StrictMode>
